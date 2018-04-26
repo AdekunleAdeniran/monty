2fc483527693e51d49636478ecf4e0ff0b4779af
Adriel Tolentino <adrieltolentino@outlook.com>
2018-04-26 21:40:09 +0000

    put newline_check in a separate file

--- tokenizer.c
+++ tokenizer.c
@@ -1,32 +1,6 @@
 #include "monty.h"
 
 /**
- * newline_check - checks if the first char is a newline after first spaces
- * @str: takes a string
- * Return: 1 if it's a new line else 0
- */
-
-int newline_check(char *str)
-{
-	int i = 0;
-
-	if (str[i] == '\n')
-		return (1);
-
-	while (str[i])
-	{
-		if (str[i] == ' ' && str[i + 1] != ' ')
-			break;
-		i++;
-	}
-	i++;
-	if (str[i] == '\n')
-		return (1);
-	else
-		return (0);
-}
-
-/**
  * tokenizer - create tokens for command and value
  * @str: takes a string
  * Return: 1 on success
