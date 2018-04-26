8034f40ed832512d9cc226d74563e973f8e67781
Adriel Tolentino <adrieltolentino@outlook.com>
2018-04-26 00:51:49 +0000

    added and modified some prototypes

---
+++ monty.h
@@ -0,0 +1,75 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define delim "\n\r "
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/** Variables */
+typedef struct variables
+{
+	char *lineptr;
+	char **lines;
+	int line_number;
+	ssize_t len;
+	char *value;
+	int int_val;
+	char *cmd;
+	int ret_val;
+} vars;
+
+extern vars gvars;
+vars gvars;
+
+
+/** Operators */
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void free_l(stack_t *head);
+
+int find_op(stack_t **head);
+int check_delim(char c);
+int tok_num(char *string);
+int tokenizer(char *str);
+
+
+#endif
