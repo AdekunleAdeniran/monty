ce371398e7971ca325c4f532dbe27d5870b1066c
adrielt07 <adriel_tolentino@outlook.com>
2018-04-27 03:46:28 +0000

    print only a-z or A-Z

--- op_pstr.c
+++ op_pstr.c
@@ -20,7 +20,7 @@ void op_pstr(stack_t **stack, __attribute__((unused))unsigned int line_number)
 	while (current != NULL)
 	{
 		check = current->n;
-		if (check >= 1 && check <= 127)
+		if ((check > 64 && check < 91) || (check > 96 && check < 123))
 			putchar(check);
 		else
 			break;
