a716f7912abd69b9a577c779f78ddca59e53e6f0
Adriel Tolentino <adrieltolentino@outlook.com>
2018-04-27 22:06:31 +0000

    remove unsued variable and used attribute unused

--- op_rotl.c
+++ op_rotl.c
@@ -2,21 +2,18 @@
 
 /**
  * op_rotl - rotates the stack to the top
- * The top element of the stack becomes the last one, and the second top
- * element of the stack becomes the first one
  * @stack: pointer to porint
  * @line_number: line number
  */
-void op_rotl(stack_t **stack, unsigned int line_number)
+void op_rotl(stack_t **stack, __attribute__((unused))unsigned int line_number)
 {
-	UNUSED(line_number);
-	int len = 0;
+/*	int len = 0; */
 	stack_t *front, *end;
 
-	len = list_len(stack);
-	if (len < 2)
+/*	len = list_len(stack); */
+/*	if (len < 2)
 		return;
-	end = *stack;
+*/	end = *stack;
 	while (end->next)
 		end = end->next;
 
