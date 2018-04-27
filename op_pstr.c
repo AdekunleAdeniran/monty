b69ff25bd62defdb1093eb3219df5a3078be8d5e
adrielt07 <adriel_tolentino@outlook.com>
2018-04-27 04:45:26 +0000

    added a helper function to help if num is in ascii value range

--- op_pstr.c
+++ op_pstr.c
@@ -1,6 +1,19 @@
 #include "monty.h"
 
 /**
+ * check_ascii - checks if int in range of ascii value except 0
+ * @num: takes an int
+ * Return: 1 if in range else 0
+ */
+int check_ascii(int num)
+{
+	if (num >= 1 && num <= 127)
+		return (1);
+	else
+		return (0);
+}
+
+/**
  * op_pstr - function to print all elements in a stack
  * @stack: double pointer of type stack_t to head of doubly linked list
  * @line_number: unsigned int for line number(void in this function)
@@ -9,22 +22,38 @@
 void op_pstr(stack_t **stack, __attribute__((unused))unsigned int line_number)
 {
 	stack_t *current;
-	int check;
+	int num, len;
 
 	if (*stack == NULL || stack == NULL)
 	{
 		putchar('\n');
 		return;
 	}
+	len = list_len(stack);
 	current = *stack;
+	if (len == 1 && check_ascii(current->n) == 1)
+	{
+		num = current->n;
+		putchar(num);
+		putchar('\n');
+		return;
+	}
+	if (check_ascii(current->n) != 1)
+		return;
 	while (current != NULL)
 	{
-		check = current->n;
-		if (check >= 1 && check <= 127)
-			putchar(check);
+		num = current->n;
+		if (check_ascii(num) == 1)
+		{
+			putchar(num);
+			current = current->next;
+		}
 		else
+		{
+			num = current->prev->n;
 			break;
-		current = current->next;
+		}
 	}
-	putchar('\n');
+	if (check_ascii(num) == 1)
+		putchar('\n');
 }
