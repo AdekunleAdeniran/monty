6b77cd3fa5bcaff280acde495dc459763ddc6f8a
flourishcodes <kunle@adeniran.com>
2018-04-26 17:55:14 -0700

    opcode to multiply elements of stack

--- monty.h
+++ monty.h
@@ -73,6 +73,7 @@ void op_add(stack_t **stack, unsigned int line_number);
 void op_nop(stack_t **stack, unsigned int line_number);
 void op_sub(stack_t **stack, unsigned int line_number);
 void op_comment(stack_t **stack, unsigned int line_number);
+void op_mul(stack_t **stack, unsigned int line_number);
 
 /* Helper */
 void free_l(stack_t *head);
