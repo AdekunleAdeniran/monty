1f38d19a21a3e0fa39d3d7f73e4d7706224fe4ae
Adriel Tolentino <adrieltolentino@outlook.com>
2018-04-27 01:49:50 +0000

    change the error message if stack is empty

--- op_pchar.c
+++ op_pchar.c
@@ -10,7 +10,7 @@ void op_pchar(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stdout, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stdout, "L%d: can't pchar, stack empty\n", line_number);
 		gvars.ret_val = -1;
 		return;
 	}
